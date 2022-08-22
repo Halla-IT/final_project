3fd5837837920dd3e1b6f100a2f30dc874827a55
Halla-it <geh4019@gmail.com>
2022-08-22 10:01:51 +0900

    프로젝트 공유

---
+++ r_project/src/test/java/com/icia/web/RProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
